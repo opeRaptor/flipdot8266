b690e2e86da09fe91a85bf91b77f3766ace1bc62
opeRaptor <testsubjectnamehere@gmail.com>
2018-07-23 00:14:52 +0100

    Python script to generate URL

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ Flipdot *flipdot = nullptr; //address Xsize Ysize
 //Flipdot flipdot1(1,2,3);
 //Flipdot flipdot2(1,2,3);
 
-std::vector<int> data = {0, 0, 0, 0, 48, 56, 440, 492, 6140, 8184, 7936, 768, 768, 768, 768, 256, 0, 0, 0, 0}; //dino
+std::vector<int> data = {0,0,0,0,48,56,440,492,6140,8184,7936,768,768,768,768,256,0,0,0,0}; //dino
 
 void setup() {
         Serial.begin(4800);
